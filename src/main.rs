08cccdcef610c65e477a1682d194d71038316f9c
Michael Weiss <dev.primeos@gmail.com>
2018-03-16 15:16:45 +0100

    Define a CspReport structure
    This structure isn't fully compatible with the current specification [0]
    yet (optional fields) and dosn't use snake case names.
    
    [0]: https://www.w3.org/TR/2016/WD-CSP3-20160913/

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate serde_derive;
+
+#[derive(Deserialize)]
+// Temporarily disable these warnings (TODO)
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+struct CspReport {
+    documentURI: String,
+    referrer: String,
+    blockedURI: String,
+    violatedDirective: String,
+    effectiveDirective: String,
+    originalPolicy: String,
+    disposition: String,
+    sourceFile: String,
+    statusCode: u16,
+    lineNumber: i32,
+    columnNumber: i32
+}
 
 #[get("/")]
 fn index() -> &'static str {
